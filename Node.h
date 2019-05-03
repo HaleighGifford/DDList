1c3e62279af3135d4e0cb064fe178d8f3a74fb6e
HaleighGifford <49070078+HaleighGifford@users.noreply.github.com>
2019-05-03 08:54:21 -0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,28 +1,4 @@
 //****************************************************************************************************
-//
-//		File:					Node.h
-//		
-//		Student:				Haleigh Cates
-//
-//		Assignment:	 			Program  #11
-//
-//		Course Name:			Data Structures I 
-//
-//		Course Number:			COSC 3050 - 01
-//
-//		Due:					December 4, 2018
-//
-//
-//		This program enahances the linked list program #6 to use a 
-//		doubly linked list.
-//
-//		Other files required:	
-//								DLList.h
-//								student.h
-//								studentList.cpp
-//								studentFile.txt
-//		
-//****************************************************************************************************
 
 #ifndef NODE_H
 #define NODE_H
